33445dafb5d1f1750f99d9fa275fc4f4022b113c
S-RR3d-7050 <dahmanishadi@fsb.u-carthage.tn>
2024-04-03 01:45:33 +0100

    Add project files.

---
+++ Entities/User.cs
@@ -0,0 +1,19 @@
+namespace WebApi.Entities;
+
+using System.Text.Json.Serialization;
+
+public class User
+{
+    public int Id { get; set; }
+    public string CIN { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string Role { get; set; }
+    //public string Username { get; set; }
+    public bool IsActive { get; set; }
+
+
+    [JsonIgnore]
+    public string PasswordHash { get; set; }
+}
---
+++ Migrations/SqlServerMigrations/20240313141125_SprintFinal.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApi.Migrations.SqlServerMigrations
+{
+    public partial class SprintFinal : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Username",
+                table: "Users",
+                newName: "Role");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CIN",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "Users",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CIN",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "Users");
+
+            migrationBuilder.RenameColumn(
+                name: "Role",
+                table: "Users",
+                newName: "Username");
+        }
+    }
+}
